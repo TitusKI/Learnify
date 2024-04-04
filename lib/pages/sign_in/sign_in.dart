1cfe284f488037339fa85fb21f24729ba9fbcb93
TitusKI <kidumazie@gmail.com>
2024-04-04 11:52:23 +0200

    working_on SignIn Screen

--- lib/pages/sign_in/sign_in.dart
+++ lib/pages/sign_in/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -10,6 +11,28 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+          child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(1.0),
+            child: Container(
+              color: Colors.grey.withOpacity(0.5),
+              height: 1.0,
+            ),
+          ),
+          title: Center(
+            child: Text('Log In',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.normal)),
+          ),
+        ),
+      )),
+    );
   }
 }
