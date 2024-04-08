7a5d13080048a96cf05a63cfb464f37ff43affe1
TitusKI <kidumazie@gmail.com>
2024-04-08 13:33:00 +0200

    Unify Routes

---
+++ lib/common/routes/names.dart
@@ -0,0 +1,10 @@
+class AppRoutes {
+  // welcome page on boarding page
+  static const INITIAL = '/';
+  // application page
+  static const APPLICATION = '/application';
+ // sign in page
+  static const SIGN_IN = '/sign_in';
+// sign up page
+  static const REGISTER = '/register';
+}
