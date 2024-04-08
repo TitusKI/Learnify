62efc1d0f69d20fe3b02496fb024578ad56accf5
TitusKI <kidumazie@gmail.com>
2024-04-08 13:34:47 +0200

    BlocProvider for All

--- lib/pages/bloc_providers.dart
+++ lib/pages/bloc_providers.dart
@@ -1,20 +1,24 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learnify/app_bloc.dart';
+import 'package:learnify/pages/register/bloc/register_bloc.dart';
 import 'package:learnify/pages/sign_in/blocs/sign_in_bloc.dart';
 import 'package:learnify/pages/welcome/blocs/bloc/welcome_bloc.dart';
 
 class AppBlocProviders {
- static get allBlocProvider => [
+  static get allBlocProvider => [
         BlocProvider(
           create: (context) => WelcomeBloc(),
         ),
+        // BlocProvider(
+        //   // lazy: false indicates that create this bloc as soon as possible
+        //   // lazy: false,
+        //   create: (context) => AppBloc(),
+        // ),
         BlocProvider(
-          // lazy: false indicates that create this bloc as soon as possible
-          // lazy: false,
-          create: (context) => AppBloc(),
+          create: (context) => SignInBloc(),
         ),
         BlocProvider(
-          create: (context)=> SignInBloc(),
-        ),
+          create: (context) => RegisterBloc(),
+        )
       ];
 }
