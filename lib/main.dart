a8e2084a69cd813e7f0081a75cecd72910c11502
TitusKI <kidumazie@gmail.com>
2024-04-08 13:32:44 +0200

    Refactor for Bloc and Route

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:learnify/app_bloc.dart';
 import 'package:learnify/app_event.dart';
 import 'package:learnify/app_state.dart';
+import 'package:learnify/common/routes/pages.dart';
 import 'package:learnify/common/values/colors.dart';
 import 'package:learnify/firebase_options.dart';
 import 'package:learnify/pages/bloc_providers.dart';
@@ -14,11 +15,9 @@ import 'package:learnify/pages/sign_in/sign_in.dart';
 import 'package:learnify/pages/welcome/blocs/bloc/welcome_bloc.dart';
 import 'package:learnify/pages/welcome/welcome.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -29,106 +28,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: AppBlocProviders.allBlocProvider,
+      providers: [...AppPages.allBlocProvider(context)],
       child: ScreenUtilInit(
         builder: (context, child) => MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              iconTheme: const IconThemeData(
-                color: AppColors.primaryText
-              ),
-
-              appBarTheme: const AppBarTheme(
-                elevation: 0,
-                backgroundColor: Colors.white,
-              ),
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            iconTheme: const IconThemeData(color: AppColors.primaryText),
+            appBarTheme: const AppBarTheme(
+              elevation: 0,
+              backgroundColor: Colors.white,
             ),
-            home: const Welcome(),
-            routes: {
-              "myHomePage": (context) => const MyHomePage(),
-              "signIn": (context) => const SignIn(),
-              "register": (context)=> const Register(),
-            }),
-      ),
-    );
-  }
-}
+          ),
+        onGenerateRoute: AppPages.GenerateRouteSettings,
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('Flutter Home Page'),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-
-        // Use of
-        child: BlocBuilder<AppBloc, AppState>(
-          builder: (context, state) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '${BlocProvider.of<AppBloc>(context).state.counter}',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            );
-          },
+          // home: const Welcome(),
+          // routes: {
+          //   // "myHomePage": (context) => const MyHomePage(),
+          //   "signIn": (context) => const SignIn(),
+          //   "register": (context) => const Register(),
+          // },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => BlocProvider.of<AppBloc>(context).add(Increment()),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
